c721a83627c45d3f7389d747c4c359b86a8ea26b
Yasin Walum <ywalum@gmail.com>
2024-04-30 09:04:58 +0300

    app

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def serve_image(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
